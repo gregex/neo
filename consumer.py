eff41b0a2fe45b1b5d5624d4f7ab71995f656f14
Gregor <gregor.orlic1@gmail.com>
2016-10-09 21:20:29 +0200

    V1.0 finished

--- consumer.py
+++ consumer.py
@@ -11,8 +11,9 @@ class consumer(SampleBase):
 		super(consumer, self).__init__(*args, **kwargs)  	
 
 	def Run(self):
+#inicijalizcija varijabli i fonta i pocetnog teksta
 		text=[]
-		text.append("NeoV1")
+		text.append("Neo")
 		uCu=[]
 		offscreenCanvas = self.matrix.CreateFrameCanvas()
 		font = graphics.Font()
@@ -20,41 +21,47 @@ class consumer(SampleBase):
 		textColor = graphics.Color(255, 165, 0)
 		pos = offscreenCanvas.width
 		myText=''
-
+#inicijalizacija konekcije na rabbitMQ
 		con = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 		chan = con.channel()
 		chan.queue_declare(queue='theclock')
-		
+
+#fukcije za gumbice		
 		def penis():
-			uCu.append('./demo -D1 -t 5 --led-chain=2 maca.ppm')
-			print('do penis')
+			uCu.append('./demo -D1 -t 5 --led-chain=2 penis.ppm')
 		def kset():
 			text.append("KSET")
-			print("do kset")
-		def matrix():
-			print('do matrix')
-		
-		animations={"Penis":penis,"Matrix":matrix,"KSET":kset}
+		def helloKitty():
+			uCu.append('./demo -D1 -t 5 --led-chain=2 maca.ppm')
 
+		animations={"Penis":penis,"Hello Kitty":helloKitty,"KSET":kset}
+
+
+#funkcija koja se zove kada dodje nesto na queue
 		def callback(ch, method, properties, body):
 			body=body.decode("utf-8")
 			if body in animations:
 				return animations[body]()
 			else:
 				text.append(body)
-					
+#konzumacija podatka s queuea					
 		chan.basic_consume(callback,queue='theclock',no_ack=True)        	     
+
+#petlja koja provjerava jel neka od lista sadrzi element i sukladno tome ispisuje element neke liste
 		while True:
 			offscreenCanvas.Clear()
-			leng = graphics.DrawText(offscreenCanvas, font, pos, 20, textColor, myText)
+			leng = graphics.DrawText(offscreenCanvas, font, pos, 30, textColor, myText)
 			pos -= 1
 			if (pos + leng < 0):
 				pos = offscreenCanvas.width
 				if len(text):
+					textColor=graphics.Color(0,255,0)
 					myText=text[0]
 					del text[0]
 				elif len(uCu):
+#brisanje matrice, jer u suprotnom sve flickera u picku materinu
 					self.matrix=''
+#izvrsavanje c koda u shellu
 					yay=call(uCu[0],shell=True)
 					del uCu[0]
 					self.matrix = RGBMatrix(self.args["rows"], self.args["chain"], self.args["parallel"])
@@ -63,10 +70,14 @@ class consumer(SampleBase):
 					offscreenCanvas = self.matrix.CreateFrameCanvas()
 					myText=time.strftime("%H:%M")
 				else:
+					textColor=graphics.Color(255,165,0)
 					myText=time.strftime("%H:%M")
+#hehe
+			if myText=="16:20" or myText=="04:20" or myText=="420":
+				textColor=graphics.Color(randint(0,255),randint(0,255),randint(0,255))
     
 			offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
-			chan.connection.process_data_events(time_limit=0.05)
+			chan.connection.process_data_events(time_limit=0.03)
 
 if __name__ == "__main__":
 	display=consumer()
